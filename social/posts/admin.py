5d01693bb52dcb5afbf008530f584fe0cc3deeee
mun-a <karnevall.13.13@gmail.com>
2021-10-09 15:12:52 +0600

    Create new model Group

--- social/posts/admin.py
+++ social/posts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Group
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -11,4 +11,14 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'slug', 'description')
+    list_display_links = ('pk', 'title')
+    search_fields = ('description', 'title')
+    list_filter = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
+    empty_value_display = '-пусто-'
+
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
--- social/posts/views.py
+++ social/posts/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from .models import Post
+from .models import Post, Group
 
 
 def index(request):
@@ -9,3 +9,13 @@ def index(request):
         'posts': posts,
     }
     return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    group_posts = group.posts.all()[:13]
+    context = {
+        'group_posts': group_posts,
+        'group': group,
+    }
+    return render(request, 'posts/group.html', context)
